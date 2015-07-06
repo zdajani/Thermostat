d1968ba31c562ada5687f28f078483a10a26a756
DuskyShelf <dupsdale@gmail.com>
2015-07-06 16:35:09 +0100

    added ability to raise temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,15 +6,20 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  describe('temperature', function(){
+  describe('temperature', function() {
 
-    it('starts at 20 degrees', function(){
+    it('starts at 20 degrees', function() {
       expect(thermostat.temperature()).toBe(20);
     });
 
     it('can be raised', function() {
       thermostat.raise();
       expect(thermostat.temperature()).toBe(21);
+    });
+
+    it('can be lowered', function() {
+      thermostat.lower();
+      expect(thermostat.temperature()).toBe(19)
     });
 
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,15 @@
 var Thermostat = function(){};
 
+var temp = 20
+
+// function Thermostat() {
+//   this.temperature = 20;
+// };
+
 Thermostat.prototype.temperature = function () {
-  return 20;
+  return temp;
+};
+
+Thermostat.prototype.raise = function() {
+  temp++;
 };
