dfe94a2ae1bea2691d31b9575430f285806baa8d
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-09 18:00:31 +0100

    current location weather

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,5 @@
 require 'sinatra/base'
 
-
-
 class ThermostatApp < Sinatra::Base
   run! if app_file == $0
 
--- app/public/application.js
+++ app/public/application.js
@@ -1,15 +1,42 @@
+// $(document).ready(function() {
+//
+//
+// });
+
+navigator.geolocation.getCurrentPosition(currentLocation);
+
+function currentLocation(Geoposition){
+    latitude = Geoposition.coords.latitude;
+    longitude = Geoposition.coords.longitude;
+    currentWeather();
+}
+
+function currentWeather(){
+  ($.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric',
+   function (response) {
+     $('#currentTemp').html(response.main.temp);
+     $('#currentCity').html(response.name);
+   }));
+}
+
+
+var thermostat = new Thermostat();
+
+
+var city = 'London';
+var latitude;
+var longitude;
+
+
+
+currentTemp();
+weatherApp();
+
 function currentTemp() {
   $('#temp').html(thermostat.temp + "&#8451");
   $('#temp').css("color", thermostat.colour);
   $('#powerSavingToggle').css("background", thermostat.powerColour);
 }
-
-var thermostat = new Thermostat();
-currentTemp();
-
-var city = 'London';
-
-weatherApp();
 
 $('#raiseTemp').click(function(){
   thermostat.raise();
@@ -35,13 +62,12 @@ $('#powerSavingToggle').click(function(){
 });
 
 function weatherApp(){
-  ($.getJSON('http://api.openweathermap.org/data/2.5/find?q=' + city + '&units=metric',
+  $.getJSON('http://api.openweathermap.org/data/2.5/find?q=' + city + '&units=metric',
    function (response) {
      $('#locationTemp').html(response.list[0].main.temp);
      $('#cityWeather').html(response.list[0].name);
-   }));
+   });
 }
-
 
 $('#citySubmit').click(function() {
   city = $('#cityForm').val();
